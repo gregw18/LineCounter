1d305e85400c3dbac903f8aa715aefc10b4ba3d5
Greg Walker <greg_walker@sympatico.ca>
2021-02-07 11:32:41 -0500

    Reading filename from args
    Am now reading file whose lines to count from command args.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+    Program to count number of files and non-blank lines in those files, in current directory and
+    all subdirectories. Can pass in pattern for files to include. i.e. *.cs. Defaults to *.* if none given.
+*/
+
+using System;
 using System.IO;
 
 using NLog;
@@ -9,7 +14,6 @@ namespace LineCounter
 {
     class Program
     {
-        private const string FileName = "FileLineCounter.cs";
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
         static void Main(string[] args)
@@ -17,8 +21,13 @@ namespace LineCounter
             Logger.Info("Starting Main");
             Logger.Info("Current working directory={Directory.GetCurrentDirectory()}", Directory.GetCurrentDirectory());
 
+            string fileName = "FileLineCounter.cs";
+            if (args.Length == 1)
+                fileName = args[0];
+            Logger.Info("Working on file {fileName}", fileName);
+
             GwLineCounter.FileLineCounter myCounter = new GwLineCounter.FileLineCounter ();
-            int numLines = myCounter.GetNumberLines (FileName);
+            int numLines = myCounter.GetNumberLines (fileName);
 
             Logger.Info("Number of lines returned is {numLines}", numLines);
         }
