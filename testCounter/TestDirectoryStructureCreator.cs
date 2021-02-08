1e1b5395850f15ac385a3cdb35594f06b2929186
Greg Walker <greg_walker@sympatico.ca>
2021-02-08 15:30:57 -0500

    Refactored TestFileTests to reduce duplication

--- testCounter/TestDirectoryStructureCreator.cs
+++ testCounter/TestDirectoryStructureCreator.cs
@@ -3,7 +3,7 @@
 
 using System.IO;
 
-using GwLineCounterTest;
+// using GwLineCounterTest;
 
 namespace GwLineCounterTest
 {
--- testCounter/TestFileTests.cs
+++ testCounter/TestFileTests.cs
@@ -12,66 +12,21 @@ namespace testCounter
 {
     public class TestFileTests
     {
-        [Fact]
-        public void TestCreateFile_MoreNonblanks()
+
+        [Theory]
+        [InlineData("test1.txt", 5, 10)]
+        [InlineData("test2.txt", 5, 2)]
+        [InlineData("test3.txt", 0, 2)]
+        [InlineData("test4.txt", 5, 0)]
+        [InlineData("test5.txt", 0, 0)]
+        public void TestCreateFile( string fileName, int numBlankLines, int numNonblankLines)
         {
-            var fileName = "test1.txt";
-            var testFile = new TestFile(fileName, 5, 10);
+            var testFile = new TestFile(fileName, numBlankLines, numNonblankLines);
             testFile.Create(Directory.GetCurrentDirectory());
 
             var myCounter = new FileLineCounter();
             int nLines = myCounter.GetNumberLines(fileName);
-            Assert.Equal(10, nLines);
+            Assert.Equal(numNonblankLines, nLines);
         }
-
-        [Fact]
-        public void TestCreateFile_MoreBlanks()
-        {
-            var fileName = "test2.txt";
-            var testFile = new TestFile(fileName, 5, 2);
-            testFile.Create(Directory.GetCurrentDirectory());
-
-            var myCounter = new FileLineCounter();
-            int nLines = myCounter.GetNumberLines(fileName);
-            Assert.Equal(2, nLines);
-        }
-
-        [Fact]
-        public void TestCreateFile_ZeroBlanks()
-        {
-            var fileName = "test3.txt";
-            var testFile = new TestFile(fileName, 0, 2);
-            testFile.Create(Directory.GetCurrentDirectory());
-
-            var myCounter = new FileLineCounter();
-            int nLines = myCounter.GetNumberLines(fileName);
-            Assert.Equal(2, nLines);
-        }
-
-        [Fact]
-        public void TestCreateFile_ZeroNonBlanks()
-        {
-            var fileName = "test4.txt";
-            var testFile = new TestFile(fileName, 5, 0);
-            testFile.Create(Directory.GetCurrentDirectory());
-
-            var myCounter = new FileLineCounter();
-            int nLines = myCounter.GetNumberLines(fileName);
-            Assert.Equal(0, nLines);
-        }
-
-        [Fact]
-        public void TestCreateFile_ZeroLines()
-        {
-            var fileName = "test5.txt";
-            var testFile = new TestFile(fileName, 0, 0);
-            testFile.Create(Directory.GetCurrentDirectory());
-
-            var myCounter = new FileLineCounter();
-            int nLines = myCounter.GetNumberLines(fileName);
-            Assert.Equal(0, nLines);
-        }
-
-
     }
 }
