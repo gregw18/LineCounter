f22dd1e9f593dcd877237aeaedff0d4976ecdb9e
Greg Walker <greg_walker@sympatico.ca>
2021-02-10 14:03:46 -0500

    Added tests for ArgParser

---
+++ testCounter/ArgParserTests.cs
@@ -0,0 +1,66 @@
+// Test argument parsing.
+
+using System;
+using System.IO;
+
+using Xunit;
+
+using GwLineCounter;
+
+namespace testArgParser
+{
+    public class ArgParserTests
+    {
+
+        string defaultRoot = ".";
+        string defaultFileSpec = "*.*";
+        
+        [Fact]
+        public void NoArguments_UsesDefaults()
+        {
+            ArgParser myParser = new ArgParser();
+            //string[] args = new string[0];
+            string[] args = {};
+            bool actualResult = myParser.ParseArgs(args);
+
+            Assert.True(actualResult);
+            Assert.Equal(defaultRoot, myParser.rootDir);
+            Assert.Equal(defaultFileSpec, myParser.fileSpec);
+        }
+
+        [Fact]
+        public void OneArgument_UsesArg()
+        {
+            ArgParser myParser = new ArgParser();
+            string[] args = {"mydir"};
+            bool actualResult = myParser.ParseArgs(args);
+
+            Assert.True(actualResult);
+            Assert.Equal("mydir", myParser.rootDir);
+            Assert.Equal(defaultFileSpec, myParser.fileSpec);
+        }
+
+        [Fact]
+        public void TwoArguments_UsesArgs()
+        {
+            ArgParser myParser = new ArgParser();
+            string[] args = {"mydir", "*.csv"};
+            bool actualResult = myParser.ParseArgs(args);
+
+            Assert.True(actualResult);
+            Assert.Equal("mydir", myParser.rootDir);
+            Assert.Equal("*.csv", myParser.fileSpec);
+        }
+
+        [Fact]
+        public void ThreeArguments_Fails()
+        {
+            ArgParser myParser = new ArgParser();
+            string[] args = {"mydir", "*.csv", "huh?"};
+            bool actualResult = myParser.ParseArgs(args);
+
+            Assert.False(actualResult);
+        }
+
+    }
+}
